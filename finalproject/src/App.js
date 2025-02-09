a12c612e220b1cbbcc69a480f16a902d8f7d3338
yooguitar <kitae1996@gmail.com>
2025-02-09 21:41:03 +0900

    auth, route, login

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/context/AuthContext";
+import Login from "./components/Auth/Login/Login";
+import KakaoMap from "./components/Kakaomap/Kakaomap";
 import Kakaomap from "./components/Kakaomap/Kakaomap";
 import MoveMap from "./components/Kakaomap/aaaaa";
+import Main from "./components/Main/Main";
+
 function App() {
   return (
     <div className="App">
-      <Kakaomap />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/map" element={<KakaoMap />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
---
+++ finalproject/src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+const Main = () => {
+  return (
+    <>
+      <h1>메인 자리</h1>
+    </>
+  );
+};
+export default Main;
